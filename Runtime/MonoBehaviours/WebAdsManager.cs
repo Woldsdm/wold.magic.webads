4849a7427653afa651dfd3dedabcc43d09257594
ali reza <lordiroh3@gmail.com>
2025-05-05 17:51:01 +0330

    .

---
+++ Runtime/MonoBehaviours/WebAdsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicWebAds
+{
+    [AddComponentMenu("Magic WebAds/WebAds Manager")]
+    public class WebAdsManager : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
